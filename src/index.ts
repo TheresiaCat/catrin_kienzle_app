16e23cd364c4850083d32a29eadace52845d2be5
on22catrinkienzle <118989508+TheresiaCat@users.noreply.github.com>
2023-04-16 17:25:05 +0200

    Renamed Todos->in todos.ts

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
-import { addTodo } from "./todos";
+import { addStuff } from "./todos";
 import { newTodoBtn, newTodoInput } from "./dom-utils";
 import { validateInput } from "./validator";
 
 function initApp() {
   newTodoBtn.disabled = true;
-  newTodoBtn.addEventListener("click", addTodo);
+  newTodoBtn.addEventListener("click", addStuff);
   newTodoInput.addEventListener("input", validateInput);
   newTodoInput.addEventListener("keydown", hasPressedEnterKeyOnTodoInput);
 }
 
 function hasPressedEnterKeyOnTodoInput(e: KeyboardEvent) {
   if (e.key === "Enter") {
-    addTodo(); // try to add a todo by pressing the enter key on the keyboard
+    addStuff(); // try to add a todo by pressing the enter key on the keyboard
   }
 }
 
--- src/todos.ts
+++ src/todos.ts
@@ -1,84 +1,85 @@
-import { newTodoInput, todosContainer } from "./dom-utils";
-import { Todo } from "./interface";
+import { newTodoInput as newStuffInput, todosContainer as stuffContainer } from "./dom-utils";
+import { Todo as Stuff } from "./interface";
 import { validateInput } from "./validator";
 
-let todos: Todo[] = [];
+let stuff: Stuff[] = [];
 
-function addTodo() {
+function addStuff() {
   if (!validateInput()) {
     return; // if input is not valid, we wont add a new todo by returning here
   }
   const timestamp = new Date();
-  const newTodo: Todo = {
-    description: newTodoInput.value,
+  const newStuff: Stuff = {
+    description: newStuffInput.value,
     // generate a new id in the format UNIXTIMESTAMP-rn-999
     id: `${timestamp.getTime()}-rn-${Math.floor(Math.random() * 999)}`,
     finished: false,
     timestamp,
   };
-  todos.push(newTodo);
-  reloadTodos();
+  stuff.push(newStuff);
+  reloadStuff();
   //empty input
-  newTodoInput.value = "";
+  newStuffInput.value = "";
 }
 
-function deleteTodo(id: string) {
-  // let all todos pass except the one with the id that will be deleted
-  todos = todos.filter((todo: Todo) => todo.id !== id);
-  reloadTodos();
+function deleteStuff(id: string) {
+  // let all stuffs pass except the one with the id that will be deleted
+  stuff = stuff.filter((todo: Stuff) => todo.id !== id);
+  reloadStuff();
 }
 
-function toggleFinishedState(id: string) {
-  todos = todos.map((todoEntry) => {
-    if (todoEntry.id === id) {
-      todoEntry.finished = !todoEntry.finished; // toggle finished State
+//ändert To-DO status 
+function ChangeStatus(id: string) { 
+  stuff = stuff.map((stuffStatus) => {
+    if (stuffStatus.id === id) {
+      stuffStatus.finished = !stuffStatus.finished; // oposite finished State
     }
-    return todoEntry;
+    return stuffStatus;
   });
-  reloadTodos();
+  reloadStuff();
 }
 
-function reloadTodos() {
-  //empty the todos list
-  todosContainer.innerHTML = "";
+function reloadStuff() {
+  //empty the stuff list
+  stuffContainer.innerHTML = "";
   // Iterate through Todos to refresh HTML
-  todos
-    .sort((todo1, todo2) => {
-      //sort todos by date
-      return todo2.timestamp.getTime() - todo1.timestamp.getTime();
+  stuff
+    .sort((stuff1, stuff2) => {
+      //sort stuff by date
+      return stuff2.timestamp.getTime() - stuff1.timestamp.getTime();
     })
-    .forEach((todo) => {
-      //create container for todo
-      const singleTodoContainer = document.createElement("div");
-      singleTodoContainer.id = todo.id;
-      singleTodoContainer.innerHTML = `
-    <p style="${todo.finished && "text-decoration: line-through;"}">
-    ${todo.description}</p>
+    .forEach((stuff) => {
+      //create container for Stuff
+      const singleStuffContainer = document.createElement("div");
+      singleStuffContainer.id = stuff.id;
+      singleStuffContainer.innerHTML = `
+    <p style="${stuff.finished && "text-decoration: line-through;"}">
+    ${stuff.description}</p>
     `;
 
       //create Delete Button
       const deleteBtn = document.createElement("button");
-      deleteBtn.addEventListener("click", () => deleteTodo(todo.id));
+      deleteBtn.addEventListener("click", () => deleteStuff(stuff.id));
       deleteBtn.style.backgroundColor = "red";
       deleteBtn.innerHTML = "X";
 
       //create finished Button
       const finishedBtn = document.createElement("button");
-      finishedBtn.addEventListener("click", () => toggleFinishedState(todo.id));
-      if (todo.finished) {
+      finishedBtn.addEventListener("click", () => ChangeStatus(stuff.id));
+      if (stuff.finished) {
         finishedBtn.innerHTML = "set unifinished";
       } else {
         finishedBtn.innerHTML = "set finished";
       }
 
       //get the delete btn in
-      singleTodoContainer.appendChild(deleteBtn);
-      singleTodoContainer.appendChild(finishedBtn);
+      singleStuffContainer.appendChild(deleteBtn);
+      singleStuffContainer.appendChild(finishedBtn);
       //append it to the wrapper
-      todosContainer.appendChild(singleTodoContainer);
+      stuffContainer.appendChild(singleStuffContainer);
       //separator between each item
-      todosContainer.appendChild(document.createElement("hr"));
+      stuffContainer.appendChild(document.createElement("hr"));
     });
 }
 
-export { addTodo, deleteTodo, reloadTodos }; 
+export { addStuff, deleteStuff, reloadStuff}; 
