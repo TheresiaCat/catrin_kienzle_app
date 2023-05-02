73aebbf0c5de9b8799d1c103706def10bef2a437
on22catrinkienzle <118989508+TheresiaCat@users.noreply.github.com>
2023-05-02 13:30:39 +0200

    added new stuff-elements

--- src/stuffs.ts
+++ src/stuffs.ts
@@ -6,17 +6,67 @@ let stuff: Stuff[] = [{
   description: "Kreditkarte",
   id: "1",
   finished: false,
-  timestamp: new Date()
+  timestamp: new Date(),
+  category: "money", 
 }];
 
-let newStuff: Stuff = {
+let geld: Stuff = {
   description: "Geld",
   id: "2",
   finished: false,
-  timestamp: new Date()
+  timestamp: new Date(),
+  category: "money", 
+}
+
+let ticket: Stuff = {
+  description: "Reiseticket",
+  id: "2",
+  finished: false,
+  timestamp: new Date(),
+  category: "papers", 
 };
 
-stuff.push(newStuff);
+let ausweis: Stuff = {
+  description: "Ausweis/Reisepass",
+  id: "2",
+  finished: false,
+  timestamp: new Date(),
+  category: "papers", 
+}
+
+let zahnbürste: Stuff = {
+  description: "Zahnbürste",
+  id: "2",
+  finished: false,
+  timestamp: new Date(),
+  category: "hygiene", 
+}
+
+let shampoo: Stuff = {
+  description: "Shampoo",
+  id: "2",
+  finished: false,
+  timestamp: new Date(),
+  category: "hygiene", 
+}
+
+let shirt: Stuff = {
+  description: "Shirt",
+  id: "2",
+  finished: false,
+  timestamp: new Date(),
+  category: "clothing", 
+}
+
+let jeans: Stuff = {
+  description: "jeans",
+  id: "2",
+  finished: false,
+  timestamp: new Date(),
+  category: "clothing", 
+}
+stuff.push(geld, ticket, ausweis,zahnbürste,shampoo,shirt,jeans);
+console.log(Stuff[]); 
 
 function addStuff() {
   if (!validateInput()) {
