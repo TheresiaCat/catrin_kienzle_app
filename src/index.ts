c627415110bac835c43ce028bdf8d37e5202d95d
on22catrinkienzle <118989508+TheresiaCat@users.noreply.github.com>
2023-05-03 11:02:14 +0200

    switched from btn class to group and created function

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,22 @@
 import { addStuff, reloadList } from "./stuffs";
-import { newStuffInput, btn} from "./dom-utils";
+import { newStuffInput, btngroup} from "./dom-utils";
 import { validateInput } from "./validator";
 
+//nur ein button freigeschaltet werden aktuell auf klassen 
 function initApp() {
   btn.disabled = true;
-  btn.addEventListener("click", addStuff);
+  for(let i=0; i<btngroup.length;i++){
+    btngroup[i].addEventListener("click",handleButtonClick);
+  }
   newStuffInput.addEventListener("input", validateInput);
   newStuffInput.addEventListener("keydown", hasPressedEnterKeyOnStuffInput);
   reloadList();
 }
 
+//add a stuff by pressing the enter key on the keyboard
 function hasPressedEnterKeyOnStuffInput(e: KeyboardEvent) {
   if (e.key === "Enter") {
-    addStuff(); // try to add a stuff by pressing the enter key on the keyboard
+    addStuff(); 
   }
 }
 
