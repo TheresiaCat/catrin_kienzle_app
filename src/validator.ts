bd76ffe42193625c03b407f93dfe0fddcf8bc00d
on22catrinkienzle <118989508+TheresiaCat@users.noreply.github.com>
2023-05-03 15:22:51 +0200

    created handleinput function

--- src/validator.ts
+++ src/validator.ts
@@ -1,14 +1,25 @@
-import {newStuffInput, btn, moneyContainer,papersContainer,hygieneContainer,clothingContainer, moneyBtn, papersBtn, hygieneBtn, clothingBtn, validatorMessage } from "./dom-utils";
+import {newStuffInput, btngroup, moneyContainer,papersContainer,hygieneContainer,clothingContainer, moneyBtn, papersBtn, hygieneBtn, clothingBtn, validatorMessage } from "./dom-utils";
 
-function validateInput() {
-  if (!newStuffInput.value) {
-    btn.disabled = true;
+//select the button to enable and to proof with input to validate
+
+function handleInput(this: HTMLInputElement) {
+  let inputid: string | null = this.getAttribute("id"); 
+  let button: HTMLButtonElement | null = null;
+  if (inputid !== null) {
+    const substring = inputid.substring(3, 8);
+    button = document.querySelector("#" + substring);
+  }
+  return button;
+}
+
+function validateInput(this: HTMLInputElement) {
+  let pressedButton: HTMLButtonElement | null = handleInput.call(this);
+  if (this.value == '') { // Kein Wert
+    if (pressedButton) pressedButton.disabled = true;
     setValidatorMessage(ValidatorMessages.INPUT_EMPTY, true);
     return false;
-  }
-  // Validated successfully
-  else {
-    btn.disabled = false;
+  } else {
+    if (pressedButton) pressedButton.disabled = false;
     setValidatorMessage(ValidatorMessages.INPUT_VALID);
     return true;
   }
@@ -29,4 +40,4 @@ const ValidatorMessages = {
   INPUT_VALID: "Great, Press the Button or 'Enter' to add",
 };
 
-export { validateInput };
+export { validateInput, handleInput };
