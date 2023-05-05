5253a1d5a7165c60f63599dd22975cfb8954f745
on22catrinkienzle <118989508+TheresiaCat@users.noreply.github.com>
2023-05-05 15:14:50 +0200

    add stuff changes fnction stucure, reload list has (container) as a property

--- src/stuffs.ts
+++ src/stuffs.ts
@@ -59,63 +59,77 @@ let jeans: Stuff = {
 }
 stuff.push(geld, ticket, ausweis,zahnbürste,shampoo,shirt,jeans);
 
-//"this" is the actual clicked button 
-function handleButtonClick(this:HTMLButtonElement){
-  let buttonid:String|null; //0 ist ausgeschlossen, dann keine rückgabe 
-  buttonid = this.getAttribute("id"); 
-  if (buttonid!==null){
-    let container: HTMLDivElement | null = document.querySelector("." + buttonid); 
-    return container; 
+//"this" is the actual clicked button
+//get the right container by the button that is clicked 
+let container: HTMLDivElement | null;
+
+function handleButtonClick(this: HTMLButtonElement) {
+  const buttonid = this.getAttribute("id");
+  if (buttonid !== null) {
+    container = document.querySelector("." + buttonid);
+    return container;
   }
 }
 
-
 function addStuff() {
-  if (!validateInput(this: HTMLInputElement)) {
-    return; // if input is not valid, we wont add a new stuff by returning here
-  }
-  const timestamp = new Date();
+  function validateInput(this: HTMLInputElement): boolean {
+    if (this.value === '') {
+      // handle empty input
+      return false;
+    }
+    // handle valid input
+    return true;
+  } 
+  
+
+  const id = `${new Date().getTime()}-rn-${Math.floor(Math.random() * 999)}`;
   const newStuff: Stuff = {
     description: newStuffInput.value,
-    // generate a new id in the format UNIXTIMESTAMP-rn-999
-    id: `${timestamp.getTime()}-rn-${Math.floor(Math.random() * 999)}`,
+    id,
     finished: false,
-    timestamp,
+    timestamp: new Date(),
   };
+
   stuff.push(newStuff);
-  reloadList();
-  //empty input
+
+  try {
+    reloadList(container);
+  } catch (error) {
+    console.error(error);
+  }
+
+  // empty input
   newStuffInput.value = "";
 }
 
 function deleteStuff(id: string) {
   // let all stuffs pass except the one with the id that will be deleted
   stuff = stuff.filter((stuff: Stuff) => stuff.id !== id);
-  reloadList();
+  reloadList(container);
 }
 
-//alters status (abgehakt?)
-function ChangeStatus(id: string) { 
+// alters status (abgehakt?)
+function ChangeStatus(id: string) {
   stuff = stuff.map((stuffStatus) => {
     if (stuffStatus.id === id) {
       stuffStatus.finished = !stuffStatus.finished; // opposite finished State
     }
     return stuffStatus;
   });
-  reloadList();
+  reloadList(container);
 }
 
-function reloadList() {//insert Parameter Array
+function reloadList(container:HTMLDivElement|null) {//insert Parameter Array
   //empty the stuff list
-  stuffsContainer.innerHTML = "";
+  if (container !== null) {//nur zugreifen wenn container definiert ist 
+    container.innerHTML = "";
+ 
   // Iterate through Stuffs to refresh HTML
   stuff.forEach((stuff) => {
       //create container for Stuff
       const singleStuffContainer = document.createElement("div");
       singleStuffContainer.id = stuff.id;
-      singleStuffContainer.innerHTML = `
-    <p style="${stuff.finished && "text-decoration: line-through;"}">
-    ${stuff.description}</p>`;
+      singleStuffContainer.innerHTML = `<p style="${stuff.finished && "text-decoration: line-through;"}">${stuff.description}</p>`;
 
       //create Delete Button
       const deleteBtn = document.createElement("button");
@@ -136,10 +150,11 @@ function reloadList() {//insert Parameter Array
       singleStuffContainer.appendChild(deleteBtn);
       singleStuffContainer.appendChild(finishedBtn);
       //append it to the wrapper
-      stuffsContainer.appendChild(singleStuffContainer);//insert in chosen container 
+      container.appendChild(singleStuffContainer);//insert in chosen container 
       //separator between each item
-      stuffsContainer.appendChild(document.createElement("hr"));
+      container.appendChild(document.createElement("hr"));
     });
+  }
 }
 
-export { addStuff, deleteStuff, reloadList, handleButtonClick, }; 
+export { addStuff, deleteStuff, reloadList, handleButtonClick, container}; 
