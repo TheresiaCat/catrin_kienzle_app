5fb0f14d8880888ec5eeb9d811b2c9e67f2e15c0
on22catrinkienzle <118989508+TheresiaCat@users.noreply.github.com>
2023-05-09 22:17:02 +0200

    fixed deleteStuff Function

--- src/editStuffElement.ts
+++ src/editStuffElement.ts
@@ -7,12 +7,26 @@ let editableStuff = [...stuff];
 //imported variables can't get assigned 
 
 
+/*
 function deleteStuff(id: string) {
     // let all stuffs pass except the one with the id that will be deleted
     editableStuff = stuff.filter((stuff: Stuff) => stuff.id !== id);
     reloadList();
   }
+  */
+
+  function deleteStuff(id: string) {
+    const index = stuff.findIndex((s) => s.id === id);
+    if (index !== -1) {
+      editableStuff.splice(index, 1); // Remove the element from the array
+      const element = document.getElementById(id);
+      if (element) {
+        element.remove(); // Remove the element from the DOM
+      }
+    }
+  }
   
+ 
   // alters status (abgehakt?)
   function ChangeStatus(id: string) {
     editableStuff = stuff.map((stuffStatus) => {
